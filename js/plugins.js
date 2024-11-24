afbc6a9e293ddfd1b4d9ea91323936598ec9bcd5
zDragonPaulo <23919@stu.ipbeja.pt>
2024-11-24 02:03:40 +0000

    Falta Corrigir a projects.html e o css.

--- js/plugins.js
+++ js/plugins.js
@@ -21,28 +21,3 @@
     }
 }());
 
-// Place any jQuery/helper plugins in here.
-$(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:30,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:1,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:true,
-                loop:false
-            }
-        }
-    })
-
-});
